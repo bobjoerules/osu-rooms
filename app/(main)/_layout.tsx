8b8586141a91598fd02d69fd6b33e3147d803abb
Miles Chase <yo.bob.joe.rules@gmail.com>
2026-05-21 00:09:15 -0700

    Update NativeTabs layout triggers to modern compound component API to restore iOS tab icons

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,6 +1,6 @@
 import * as Haptics from 'expo-haptics';
 import { Tabs, useRouter, useSegments } from 'expo-router';
-import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
 import { useEffect, useRef } from 'react';
 import { Platform } from 'react-native';
 import CustomTabBar from '../../components/CustomTabBar';
@@ -54,30 +54,30 @@ export default function TabLayout() {
 
     const triggers = [
         <NativeTabs.Trigger key="index" name="index">
-            <Label>Rooms</Label>
-            <Icon sf="square.grid.2x2.fill" drawable="ic_menu_home" />
+            <NativeTabs.Trigger.Label>Rooms</NativeTabs.Trigger.Label>
+            <NativeTabs.Trigger.Icon sf="square.grid.2x2.fill" drawable="ic_menu_home" />
         </NativeTabs.Trigger>,
         showSubmitTab ? (
             <NativeTabs.Trigger key="add" name="add">
-                <Label>Add/Edit</Label>
-                <Icon sf="plus.circle.fill" drawable="ic_input_add" />
+                <NativeTabs.Trigger.Label>Add/Edit</NativeTabs.Trigger.Label>
+                <NativeTabs.Trigger.Icon sf="plus.circle.fill" drawable="ic_input_add" />
             </NativeTabs.Trigger>
         ) : null,
         useBetaFeatures ? (
             <NativeTabs.Trigger key="osu" name="osu">
-                <Label>OSU</Label>
-                <Icon sf="link" drawable="ic_menu_share" />
+                <NativeTabs.Trigger.Label>OSU</NativeTabs.Trigger.Label>
+                <NativeTabs.Trigger.Icon sf="link" drawable="ic_menu_share" />
             </NativeTabs.Trigger>
         ) : null,
         showReviewsTab ? (
             <NativeTabs.Trigger key="reviews" name="reviews">
-                <Label>Reviews</Label>
-                <Icon sf="star.bubble.fill" drawable="ic_menu_edit" />
+                <NativeTabs.Trigger.Label>Reviews</NativeTabs.Trigger.Label>
+                <NativeTabs.Trigger.Icon sf="star.bubble.fill" drawable="ic_menu_edit" />
             </NativeTabs.Trigger>
         ) : null,
         <NativeTabs.Trigger key="account" name="account">
-            <Label>Account</Label>
-            <Icon sf="person.fill" drawable="ic_menu_allfriends" />
+            <NativeTabs.Trigger.Label>Account</NativeTabs.Trigger.Label>
+            <NativeTabs.Trigger.Icon sf="person.fill" drawable="ic_menu_allfriends" />
         </NativeTabs.Trigger>
     ].filter(Boolean);
 
